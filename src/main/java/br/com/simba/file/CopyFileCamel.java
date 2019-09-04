88b2d1c8162f1958a9231b3957ed6b92f330d5dd
Ícaro Oliveira <icarotadeu@hotmail.com>
2019-09-03 23:04:51 -0300

    Criação do projeto para testar o Camel

---
+++ src/main/java/br/com/simba/file/CopyFileCamel.java
@@ -0,0 +1,32 @@
+package br.com.simba.file;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFileCamel {
+    public static void main(String[] args) {
+        CamelContext camelContext = new DefaultCamelContext();
+
+        try {
+            camelContext.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("file:data/input?noop=true")
+                            .to("log:?level=INFO&showBody=true&showHeaders=true")
+                            .to("file:data/output");
+                }
+            });
+
+            camelContext.start();
+
+            Thread.sleep(5000);
+
+            camelContext.stop();
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            e.printStackTrace();
+        }
+    }
+}
